4e76bf2cabab1374b858ff4e667dd72ca26b6a6e
GianniBalistreri <gbalistreri@gmx.de>
2021-03-12 15:49:42 +0100

    Update version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [r.strip() for r in requires.split('\n') if ((r.strip()[0] != "#") an
 
 setuptools.setup(
     name='happy_learning',
-    version='0.2.5',
+    version='0.2.6',
     author='Gianni Francesco Balistreri',
     author_email='gbalistreri@gmx.de',
     description='Toolbox for reinforced developing of supervised machine learning models (as proof-of-concept)',
