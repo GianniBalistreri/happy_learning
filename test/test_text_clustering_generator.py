fd5b2806a9bb99b26c47b56c82d1fb8fcf8b6480
GianniBalistreri <gbalistreri@gmx.de>
2021-03-18 12:21:28 +0100

    Add unittest for class ClusterGenerator

--- test/test_text_clustering_generator.py
+++ test/test_text_clustering_generator.py
@@ -1,8 +1,12 @@
+import copy
 import unittest
 
 from gensim import corpora
 from happy_learning.text_clustering import GibbsSamplingDirichletMultinomialModeling, LatentDirichletAllocation, LatentSemanticIndexing, NonNegativeMatrixFactorization
-from happy_learning.text_clustering_generator import Clustering, ClusteringGenerator
+from happy_learning.text_clustering_generator import CLUSTER_ALGORITHMS, Clustering, ClusteringGenerator
+
+DATA_FILE_PATH: str = 'data/tweets.csv'
+N_CLUSTERS: int = 10
 
 
 class ClusteringTest(unittest.TestCase):
@@ -10,7 +14,8 @@ class ClusteringTest(unittest.TestCase):
     Class for testing class Clustering
     """
     def test_gibbs_sampling_dirichlet_multinomial_model(self):
-        self.assertTrue(expr=isinstance(GibbsSamplingDirichletMultinomialModeling(n_clusters=8,
+        self.assertTrue(expr=isinstance(GibbsSamplingDirichletMultinomialModeling(vocab_size=0,
+                                                                                  n_clusters=8,
                                                                                   n_iterations=30,
                                                                                   alpha=0.1,
                                                                                   beta=0.1
@@ -59,8 +64,6 @@ class ClusteringTest(unittest.TestCase):
 
     def test_non_negative_matrix_factorization(self):
         self.assertTrue(expr=isinstance(NonNegativeMatrixFactorization(lang='en',
-                                                                       doc_term_matrix=[],
-                                                                       vocab=corpora.Dictionary(documents=[], prune_at=2000000),
                                                                        n_clusters=10,
                                                                        n_iterations=200
                                                                        ),
@@ -75,27 +78,128 @@ class ClusteringTest(unittest.TestCase):
                                                                                       ).non_negative_matrix_factorization_param().get(list(_nmf_param.keys())[0]))
 
 
-class ClusterNetworkTest(unittest.TestCase):
+class ClusteringGeneratorTest(unittest.TestCase):
     """
     Class for testing class ClusteringGenerator
     """
-    def test_generate_model(self):
-        _net_gen: object = ClusteringGenerator(predictor='',
+    def test_generate_gibbs_sampling_dirichlet_multinomial_model(self):
+        _net_gen: object = ClusteringGenerator(predictor='tweet',
                                                model_name=None,
                                                cluster_params=None,
                                                models=['gsdmm'],
-                                               tokenize=False,
+                                               tokenize=True,
                                                random=True,
-                                               sep='\t',
-                                               cloud=None
+                                               sep=',',
+                                               cloud=None,
+                                               train_data_path=DATA_FILE_PATH
                                                ).generate_model()
         self.assertTrue(expr=isinstance(_net_gen.model, GibbsSamplingDirichletMultinomialModeling))
 
+    def test_generate_latent_dirichlet_allocation_model(self):
+        _net_gen: object = ClusteringGenerator(predictor='tweet',
+                                               model_name=None,
+                                               cluster_params=None,
+                                               models=['lda'],
+                                               tokenize=False,
+                                               random=True,
+                                               sep=',',
+                                               cloud=None,
+                                               train_data_path=DATA_FILE_PATH
+                                               ).generate_model()
+        self.assertTrue(expr=isinstance(_net_gen.model, LatentDirichletAllocation))
+
+    def test_generate_latent_semantic_indexing_model(self):
+        _net_gen: object = ClusteringGenerator(predictor='tweet',
+                                               model_name=None,
+                                               cluster_params=None,
+                                               models=['lsi'],
+                                               tokenize=False,
+                                               random=True,
+                                               sep=',',
+                                               cloud=None,
+                                               train_data_path=DATA_FILE_PATH
+                                               ).generate_model()
+        self.assertTrue(expr=isinstance(_net_gen.model, LatentSemanticIndexing))
+
+    def test_generate_non_negative_matrix_factorization_model(self):
+        _net_gen: object = ClusteringGenerator(predictor='tweet',
+                                               model_name=None,
+                                               cluster_params=None,
+                                               models=['nmf'],
+                                               tokenize=False,
+                                               random=True,
+                                               sep=',',
+                                               cloud=None,
+                                               train_data_path=DATA_FILE_PATH
+                                               ).generate_model()
+        self.assertTrue(expr=isinstance(_net_gen.model, NonNegativeMatrixFactorization))
+
     def test_generate_params(self):
-        pass
+        _model_generator: ClusteringGenerator = ClusteringGenerator(predictor='tweet',
+                                                                    model_name=None,
+                                                                    cluster_params=None,
+                                                                    models=list(CLUSTER_ALGORITHMS.keys()),
+                                                                    sep=',',
+                                                                    train_data_path=DATA_FILE_PATH,
+                                                                    )
+        _model = _model_generator.generate_model()
+        _mutated_param: dict = copy.deepcopy(_model.model_param_mutated)
+        _model_generator.generate_params(param_rate=0.1, force_param=None)
+        self.assertTrue(expr=len(_mutated_param.keys()) < len(_model_generator.model_param_mutated.keys()))
 
     def test_get_model_parameter(self):
-        pass
+        self.assertTrue(expr=len(ClusteringGenerator(predictor='tweet',
+                                                     model_name=None,
+                                                     cluster_params=None,
+                                                     models=list(CLUSTER_ALGORITHMS.keys())
+                                                     ).get_model_parameter().keys()
+                                 ) > 0
+                        )
 
-    def test_train(self):
-        pass
+    def test_train_gibbs_sampling_dirichlet_multinomial_model(self):
+        _model_generator: ClusteringGenerator = ClusteringGenerator(predictor='tweet',
+                                                                    model_name=None,
+                                                                    cluster_params=None,
+                                                                    models=['gsdmm'],
+                                                                    sep=',',
+                                                                    train_data_path=DATA_FILE_PATH,
+                                                                    )
+        _model = _model_generator.generate_model()
+        _model.train()
+        self.assertTrue(expr=len(_model.cluster_label) > 0 and _model.nmi > 0.0)
+
+    def test_train_latent_dirichlet_allocation(self):
+        _model_generator: ClusteringGenerator = ClusteringGenerator(predictor='tweet',
+                                                                    model_name=None,
+                                                                    cluster_params=None,
+                                                                    models=['lda'],
+                                                                    sep=',',
+                                                                    train_data_path=DATA_FILE_PATH,
+                                                                    )
+        _model = _model_generator.generate_model()
+        _model.train()
+        self.assertTrue(expr=len(_model.cluster_label) > 0 and _model.nmi > 0.0)
+
+    def test_train_latent_semantic_indexing(self):
+        _model_generator: ClusteringGenerator = ClusteringGenerator(predictor='tweet',
+                                                                    model_name=None,
+                                                                    cluster_params=None,
+                                                                    models=['lsi'],
+                                                                    sep=',',
+                                                                    train_data_path=DATA_FILE_PATH,
+                                                                    )
+        _model = _model_generator.generate_model()
+        _model.train()
+        self.assertTrue(expr=len(_model.cluster_label) > 0 and _model.nmi > 0.0)
+
+    def test_train_non_negative_matrix_factorization(self):
+        _model_generator: ClusteringGenerator = ClusteringGenerator(predictor='tweet',
+                                                                    model_name=None,
+                                                                    cluster_params=None,
+                                                                    models=['nmf'],
+                                                                    sep=',',
+                                                                    train_data_path=DATA_FILE_PATH,
+                                                                    )
+        _model = _model_generator.generate_model()
+        _model.train()
+        self.assertTrue(expr=len(_model.cluster_label) > 0 and _model.nmi > 0.0)
